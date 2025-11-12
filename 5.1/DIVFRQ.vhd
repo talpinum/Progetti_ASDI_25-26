4dd63e45a2864b20f5203a2af3afd28340b28b39
Fabrizio <144843999+Fabronsky@users.noreply.github.com>
2025-11-12 18:43:03 +0100

    Create DIVFRQ.vhd

---
+++ 5.1/DIVFRQ.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12.11.2025 18:41:51
+-- Design Name: 
+-- Module Name: DIVFRQ - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity DIVFRQ is
+    Port ( clk_in : in STD_LOGIC;
+           clk_out : out STD_LOGIC;
+           rst : in STD_LOGIC);
+end DIVFRQ;
+
+architecture Behavioral of DIVFRQ is
+
+begin
+
+
+end Behavioral;
