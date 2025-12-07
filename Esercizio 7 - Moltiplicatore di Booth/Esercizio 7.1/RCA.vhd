0c00c464d7e6261f59b0c094e2f45fdf03ab53b8
Fabrizio <144843999+Fabronsky@users.noreply.github.com>
2025-12-07 23:01:48 +0100

    Update RCA.vhd

--- Esercizio 7 - Moltiplicatore di Booth/Esercizio 7.1/RCA.vhd
+++ Esercizio 7 - Moltiplicatore di Booth/Esercizio 7.1/RCA.vhd
@@ -1,74 +1,31 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 07.12.2025 16:08:55
--- Design Name: 
--- Module Name: RCA - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+entity ripple_carry is
+	port( X, Y: in std_logic_vector(7 downto 0);
+		  c_in: in std_logic;
+		  c_out: out std_logic;
+		  Z: out std_logic_vector(7 downto 0));
+end ripple_carry;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity RCA is
-  Port (
-      x : in std_logic_vector(7 downto 0);
-      y : in std_logic_vector(7 downto 0);
-      c_in : in std_logic;
-      c_out : out std_logic;
-      z : out std_logic_vector(7 downto 0)
-  );
-end RCA;
-
-architecture Structural of RCA is
-
-component FA is
-    port(
-      a : in std_logic;
-      b : in std_logic;
-      c_in : in std_logic;
-      c_out : out std_logic;
-      s : out std_logic
-    );
-end component;
-
-    signal c : std_logic_vector(8 downto 0);
-
-begin
-    
-    c(0) <= c_in;
-    
-    gen_FA : for i in 0 to 7 generate
-        FA_i : FA
-            port map(
-                a     => x(i),
-                b     => y(i),
-                c_in  => c(i),
-                s     => z(i),
-                c_out => c(i+1)
-            );
-    end generate;
-    
-    c_out <= c(8);
-end Structural;
+architecture structural of ripple_carry is
+	component full_adder is 
+	port(
+	a,b: in std_logic; 
+	cin: in std_logic;
+	cout, s: out std_logic);
+	end component;
+	
+	signal temp: std_logic_vector(7 downto 0);
+	
+	begin
+	
+	RA0: full_adder port map(X(0), Y(0), c_in, temp(0), Z(0));
+	
+	RA1to6: FOR i IN 1 TO 6 GENERATE
+			RA: full_adder port map(X(i), Y(i), temp(i-1), temp(i), Z(i));
+			END GENERATE;
+			
+	RA7: full_adder port map(X(7), Y(7), temp(6), c_out, Z(7));
+	
+	end structural;
