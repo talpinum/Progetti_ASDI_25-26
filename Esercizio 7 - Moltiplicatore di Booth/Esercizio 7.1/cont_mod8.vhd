d0b1c820a0449d52a0cd85c944bd3fe381b1081e
Fabrizio <144843999+Fabronsky@users.noreply.github.com>
2025-12-07 23:01:34 +0100

    Update cont_mod8.vhd

--- Esercizio 7 - Moltiplicatore di Booth/Esercizio 7.1/cont_mod8.vhd
+++ Esercizio 7 - Moltiplicatore di Booth/Esercizio 7.1/cont_mod8.vhd
@@ -1,61 +1,34 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 07.12.2025 16:08:55
--- Design Name: 
--- Module Name: cont_mod8 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+--use IEEE.std_logic_unsigned.ALL;
 
 entity cont_mod8 is
-  Port (
-  clk : in std_logic;
-  rst : in std_logic;
-  A : in std_logic;
-  count : out std_logic_vector(2 downto 0)
-  );
+	port( clock, reset: in std_logic;
+	      count_in: in std_logic;
+		  count: out std_logic_vector(2 downto 0));
 end cont_mod8;
 
-architecture Behavioral of cont_mod8 is
-
-    signal c : unsigned(2 downto 0) := (Others => '0');
-begin
-
-    process(clk)
-    begin
-        if rising_edge(clk) then
-            if rst = '1' then
-                c <= (Others => '0');
-            elsif A = '1' then
-                    c <= c + 1;
-            end if;    
-        end if;    
-    end process;
-    
-    count <= std_logic_vector(c);           
-                
-end Behavioral;
+architecture behavioural of cont_mod8 is
+	signal c: std_logic_vector(2 downto 0) := (others=>'0');
+	
+	begin	
+	CM8: process(clock)
+	begin
+		if(clock'event and clock='1') then 
+		
+		   if(reset='1') then 
+			   c <= (others=>'0');
+		   else 
+		       if(count_in = '1') then
+			      c <= std_logic_vector(unsigned(c) + 1);
+			      -- c <= c + "111"; posso fare direttamente questa somma se importo IEEE.std_logic_unisigned.ALL
+			      -- è preferibile non farlo perchè sono package non standard
+		       end if;
+		   end if;
+		 end if;
+	end process;
+	
+	count <= c;
+	
+	end behavioural;
