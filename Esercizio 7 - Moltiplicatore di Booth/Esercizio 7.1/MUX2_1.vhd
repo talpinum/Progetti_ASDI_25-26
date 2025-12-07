f87bf457d8b91a5f0ff0573a5b519ddd2f0bbfaa
Fabrizio <144843999+Fabronsky@users.noreply.github.com>
2025-12-07 23:01:21 +0100

    Update MUX2_1.vhd

--- Esercizio 7 - Moltiplicatore di Booth/Esercizio 7.1/MUX2_1.vhd
+++ Esercizio 7 - Moltiplicatore di Booth/Esercizio 7.1/MUX2_1.vhd
@@ -1,54 +1,24 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 07.12.2025 16:08:55
--- Design Name: 
--- Module Name: MUX2_1 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity MUX2_1 is
-  generic(
-    width : INTEGER := 8
-  );
-  Port (
-  x0 : in std_logic_vector(width-1 downto 0);
-  x1 : in std_logic_vector(width-1 downto 0);
-  s : in std_logic;
-  y : out std_logic_vector(width-1 downto 0)
-  );
-end MUX2_1;
-
-architecture rtl of MUX2_1 is
+entity mux_21 is
+    generic (width : integer range 0 to 17 := 8);
+	port( x0, x1: in std_logic_vector(width-1 downto 0); 
+		  s: in std_logic;
+		  y: out std_logic_vector(width-1 downto 0));
+end mux_21;
 
 
-begin
+architecture rtl of mux_21 is
 
-    y <= x0 when s='0' else
-         x1 when s='1' else
-         (others => '0');
-end rtl;
+	begin
+	
+	y <= x0 when s='0' else 
+	    x1 when s='1' else
+		(others => '0');
+    end rtl;
+
+		
+		
+		
