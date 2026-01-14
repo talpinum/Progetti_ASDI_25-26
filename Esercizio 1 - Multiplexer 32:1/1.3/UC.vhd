5e11c0eeb9d85bbbefe9019a1b287274fc6cae8d
Fabrizio <144843999+Fabronsky@users.noreply.github.com>
2026-01-14 16:06:02 +0100

    Create UC.vhd

---
+++ Esercizio 1 - Multiplexer 32:1/1.3/UC.vhd
@@ -0,0 +1,70 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 13.01.2026 17:50:30
+-- Design Name: 
+-- Module Name: UC - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity UC is
+  Port (
+  clk : in std_logic;
+  rst : in std_logic;
+  firstbutton : in std_logic;
+  secondbutton : in std_logic;
+  switch : in std_logic_vector(15 downto 0);
+  x_out : out std_logic_vector(31 downto 0)
+   );
+end UC;
+
+architecture Behavioral of UC is
+
+    signal first_reg : std_logic_vector(15 downto 0) := (others => '0');
+    signal second_reg : std_logic_vector(15 downto 0) := (others => '0');
+
+begin
+
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if rst = '1' then
+                first_reg <= (others => '0');
+                second_reg <= (others => '0');
+                
+            elsif firstbutton = '1' then
+                first_reg <= switch;
+                
+            elsif secondbutton = '1' then
+                second_reg <= switch;
+            end if;
+            end if;
+    end process;
+    
+    x_out <=  first_reg & second_reg;
+
+end Behavioral;
