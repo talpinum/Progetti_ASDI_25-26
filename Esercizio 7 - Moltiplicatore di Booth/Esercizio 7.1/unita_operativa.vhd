0104c36960a5ea177e2198cd310fad224433e674
Fabrizio <144843999+Fabronsky@users.noreply.github.com>
2026-01-20 20:18:34 +0100

    Update unita_operativa.vhd

--- Esercizio 7 - Moltiplicatore di Booth/Esercizio 7.1/unita_operativa.vhd
+++ Esercizio 7 - Moltiplicatore di Booth/Esercizio 7.1/unita_operativa.vhd
@@ -73,14 +73,14 @@ begin
     
     --stringa da 16 bit da inserire nello shift register A.Q durante la fare di inizializzazone:
     --è ottenuta concatenando 00000000 con il moltiplicatore X
-    AQ_init <= "00000000" & X;  --valore da inserire all'inizio nello shift register
+    AQ_init <= "00000000" & X & "0";  --valore da inserire all'inizio nello shift register
     
     --stringa di 16 bit da inserire nello shift register A.Q durante la fase operativa dopo aver effettuato la somma
-    AQ_sum_in <=sum & AQ_out(7 downto 0);  
+    AQ_sum_in <=sum & AQ_out(8 downto 0);  
     
     -- mux per selezionare l'ingresso parallelo dello shift register: valore iniziale AQ_init 
     -- oppure uscita dell'adder AQ_sum_in
-    MUX_SR_parallel_in : mux_21 generic map (width => 16) port map(AQ_init, AQ_sum_in, selAQ, AQ_in);
+    MUX_SR_parallel_in : mux_21 generic map (width => 17) port map(AQ_init, AQ_sum_in, selAQ, AQ_in);
     
     
     -- 3) predisposizione dell'ingresso seriale dello shift register: deve prendere sempre F tranne che nel
