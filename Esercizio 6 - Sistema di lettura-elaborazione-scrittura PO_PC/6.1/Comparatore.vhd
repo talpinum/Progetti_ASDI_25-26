abf8312ca9100c0791cf4f61c437cdd8eda1ab86
Fabrizio <144843999+Fabronsky@users.noreply.github.com>
2026-01-29 18:12:56 +0100

    Update Comparatore.vhd

--- Esercizio 6 - Sistema di lettura-elaborazione-scrittura PO_PC/6.1/Comparatore.vhd
+++ Esercizio 6 - Sistema di lettura-elaborazione-scrittura PO_PC/6.1/Comparatore.vhd
@@ -31,102 +31,20 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
--- LE COSE COMMENTATE SONO SE LO SI VUOLE FARE SINCRONO MA PER ME E' UNA PUTTANATA
-LEGGETE SOPRA CHE LO SO CHE VI ESCE UN BELL'ERRORE SU VIVADO
 entity Comparatore is
   Port (
     a : in std_logic_vector(7 downto 0);
-    x : in std_logic_vector(7 downto 0);
+    --x : in std_logic_vector(7 downto 0);
     y : out std_logic
-    -- clk
-    -- rst
   );
 end Comparatore;
 
 architecture Behavioral of Comparatore is
- -- signal eq_reg : std_logic := '0';
+
+    constant X : std_logic_vector(7 downto 0) := "10101010";
+    
 begin
+
     y <= '1' when a = x else '0';
 
---process(clk, rst)
---    begin
---        if rst = '1' then
---            eq_reg <= '0';
---        elsif rising_edge(clk) then
---           if a = x then
---                eq_reg <= '1';
---            else
---                eq_reg <= '0';
---            end if;
---       end if;
---    end process;
-
---    y <= eq_reg;
-
 end Behavioral;
-
-
-
-
-
-
-  l--ibrary IEEE;
---use IEEE.STD_LOGIC_1164.ALL;
-
---entity Comparatore is
-  --generic (
-    --X_REF : std_logic_vector(7 downto 0) := "10101010"
-  --);
-  --port (
-    --a : in  std_logic_vector(7 downto 0);
-    --y : out std_logic
-  --);
---end Comparatore;
-
---architecture Behavioral of Comparatore is
---begin
-  --  y <= '1' when a = X_REF else '0';
---end Behavioral;
-
-
-
-
-
-  ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 15.11.2025 17:40:41
--- Design Name: 
--- Module Name: Comparatore - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: Comparatore 8-bit con stringa X precaricata come COSTANTE interna.
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
-entity Comparatore is
-  Port (
-    a : in  std_logic_vector(7 downto 0);
-    y : out std_logic
-  );
-end Comparatore;
-
-architecture Behavioral of Comparatore is
-    -- Stringa X precaricata (modifica qui il valore desiderato)
-    constant X_REF : std_logic_vector(7 downto 0) := "10101010";
-begin
-    -- Comparatore combinatorio
-    y <= '1' when a = X_REF else '0';
-end Behavioral;
-
