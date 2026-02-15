9bbd81ce1a49c406fbef0a3049356ed59a384627
Fabrizio <144843999+Fabronsky@users.noreply.github.com>
2026-02-15 23:18:48 +0100

    Update U_O_B_Completa.vhd

--- Esercizio 8 - Comunicazione con handshaking/8.2/U_O_B_Completa.vhd
+++ Esercizio 8 - Comunicazione con handshaking/8.2/U_O_B_Completa.vhd
@@ -31,15 +31,24 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
 entity U_O_B_Completa is
     Port (
     clk_o : in std_logic;
     rst_o : in std_logic;
     load : in  std_logic;
     data_out : out std_logic_vector(4 downto 0);
-    ok_user_B : out std_logic; -- verso A
-    okUser : in std_logic; -- segnale utente esterno
-    clear_okUser : in  std_logic;-- da UC: reset registro okUser
     data_in : in std_logic_vector(7 downto 0)
     );
 end U_O_B_Completa;
@@ -62,45 +71,38 @@ architecture Behavioral of U_O_B_Completa is
         D : out std_logic_vector(3 downto 0)
         );
     end component;
+
+     component registro_pipo is
+        generic (
+            N : integer := 8
+        );
+        port (
+            clk   : in  std_logic;
+            rst   : in  std_logic;
+            load  : in  std_logic;
+            d_in  : in  std_logic_vector(N-1 downto 0);
+            q_out : out std_logic_vector(N-1 downto 0)
+        );
+    end component;
     
     signal data_reg : std_logic_vector(7 downto 0) := (others => '0');
     signal sinistra, destra : std_logic_vector(3 downto 0);
     signal risultato : std_logic_vector(4 downto 0);
-    signal okUser_reg : std_logic := '0';
   
 
 begin
     
-    process(clk_o, rst_o)
-    begin
-        if rising_edge(clk_o) then
-            if rst_o = '1' then
-                data_reg <= (others => '0');
-                
-            else
-                if load = '1' then
-                    data_reg <= data_in;
-                end if;
-            end if;
-        end if;
-    end process;
-    
-    process(clk_o, rst_o)
-    begin
-        if rising_edge(clk_o) then
-            if rst_o = '1' then
-                okUser_reg <= '0';
-            else
-                if okUser = '1' then
-                    okUser_reg <= '1';
-                end if;
-                if clear_okUser = '1' then -- quando la UC ha concluso l'elaborazione, azzera
-                    okUser_reg <= '0';
-                end if;
-
-            end if;
-        end if;
-    end process;
+    REG_B : registro_pipo
+        generic map(
+            N => 8
+        )
+        port map(
+            clk   => clk_o,
+            rst   => rst_o,
+            load  => load,
+            d_in  => data_in,
+            q_out => data_reg
+        );
     
     Sommatore : Sommatore4
         Port map(
@@ -117,7 +119,6 @@ begin
         );
     
     data_out <= risultato;
-    ok_user_B <= okUser_reg;
     
 
 end Behavioral;
