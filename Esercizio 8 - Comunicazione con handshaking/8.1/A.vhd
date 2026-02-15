35f409cba7600963b1137b3bd9b5cd93611e481e
Fabrizio <144843999+Fabronsky@users.noreply.github.com>
2026-02-15 22:46:36 +0100

    Update A.vhd

--- Esercizio 8 - Comunicazione con handshaking/8.1/A.vhd
+++ Esercizio 8 - Comunicazione con handshaking/8.1/A.vhd
@@ -65,17 +65,17 @@ architecture Structural of A is
     Port (
         clk_o : in std_logic;
         rst_o : in std_logic;
-        read : in std_logic;
+        load_reg : in std_logic;
         A_cont : in std_logic;
         last_o : out std_logic;
         Y : out std_logic_vector(7 downto 0)
     );
  end component;
  
-    signal last_sig  : std_logic;
+    signal last_sig : std_logic;
     signal A_cont_sig: std_logic;
-    signal done_sig  : std_logic;
-    signal data_sig  : std_logic_vector(7 downto 0);
+    signal load_sig : std_logic;
+    signal data_sig : std_logic_vector(7 downto 0);
     
 begin
 
@@ -99,7 +99,7 @@ begin
     port map(
         clk_o => clk_A,
         rst_o => rst_A,
-        read => read_sig,
+        load_reg => load_sig,
         A_cont => A_cont_sig,
         last_o => last_sig,
         Y => data_sig
