7fb36de313ac3b650bb7799fa12b997a16057abf
Fabrizio <144843999+Fabronsky@users.noreply.github.com>
2026-02-15 23:38:48 +0100

    Update B_Completo.vhd

--- Esercizio 8 - Comunicazione con handshaking/8.2/B_Completo.vhd
+++ Esercizio 8 - Comunicazione con handshaking/8.2/B_Completo.vhd
@@ -10,30 +10,30 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity B_Completo is
+eentity B_Completo is
     Port (
         clkB : in std_logic;
         rstB : in std_logic;
-        req : in std_logic; -- da nodo A
-        data_in : in std_logic_vector(7 downto 0); -- da nodo A
+        req : in std_logic;                      -- da nodo A
+        data_in : in std_logic_vector(7 downto 0);
         ack : out std_logic;
-        okUser : in std_logic;
-        ok_user_B : out std_logic;
+        okUser : in std_logic;                   -- input utente
+        ok_user_B : out std_logic;               -- verso esterno
         resultB : out std_logic_vector(4 downto 0)
     );
 end B_Completo;
     
-
-    
 architecture Structural of B_Completo is
-        component Unita_Controllo_B_Completo
+
+    component Unita_Controllo_B_Completo
         Port (
             clk : in std_logic;
             rst : in std_logic;
+            okUser : in std_logic;
             load : out std_logic;
             req : in std_logic;
-            okUser : in std_logic; -- dall'utente
-            -- clear_okUser : out std_logic; -- per UO
+            done : out std_logic;
+            user_ack : out std_logic;
             ack : out std_logic
         );
     end component;
@@ -44,45 +44,44 @@ architecture Structural of B_Completo is
             rst_o : in std_logic;
             load : in std_logic;
             data_out : out std_logic_vector(4 downto 0);
-            ok_user_B : out std_logic; 
-            okUser : in  std_logic; 
-            -- clear_okUser : in  std_logic;
             data_in : in std_logic_vector(7 downto 0)
         );
     end component;
-    
-    signal load_sig : std_logic;
-    signal ack_sig : std_logic;
-    signal risultato : std_logic_vector(4 downto 0);
-    -- signal clear_okUser_s : std_logic;
+
+    signal load_sig    : std_logic;
+    signal ack_sig     : std_logic;
+    signal done_sig    : std_logic;
+    signal user_ack_sig: std_logic;
+    signal risultato   : std_logic_vector(4 downto 0);
+
 begin
 
+    -- UNITÀ DI CONTROLLO
     UC_B_C : Unita_Controllo_B_Completo
         port map(
             clk => clkB,
             rst => rstB,
+            okUser => okUser,
             load => load_sig,
             req => req,
-            okUser => okUser,
-            -- clear_okUser => clear_okUser_s,
+            done => done_sig,
+            user_ack => user_ack_sig,
             ack => ack_sig
         );
 
+    -- UNITÀ OPERATIVA
     UO_B_C : U_O_B_Completa
         port map(
             clk_o => clkB,
             rst_o => rstB,
             load => load_sig,
             data_out => risultato,
-            ok_user_B => ok_user_B,
-            okUser => okUser,
-            -- clear_okUser => clear_okUser_s,
             data_in => data_in
         );
 
+    -- USCITE ESTERNE
     ack <= ack_sig;
     resultB <= risultato;
-    
+    ok_user_B <= user_ack_sig;
+
 end Structural;
-
-
