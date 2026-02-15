b01d09a7b0ff88e7b816f008d77717ab74a20118
Fabrizio <144843999+Fabronsky@users.noreply.github.com>
2026-02-15 23:36:01 +0100

    Update Unita_Controllo_B_Completo.vhd

--- Esercizio 8 - Comunicazione con handshaking/8.2/Unita_Controllo_B_Completo.vhd
+++ Esercizio 8 - Comunicazione con handshaking/8.2/Unita_Controllo_B_Completo.vhd
@@ -36,15 +36,17 @@ entity Unita_Controllo_B_Completo is
     clk : in std_logic;
     rst : in std_logic;
     okUser : in std_logic;
-    load : out std_logic; -- dice all'UO di campionare il byte
+    load : out std_logic; 
     req : in std_logic;
+    done : out std_logic;
+    user_ack : out std_logic;
     ack : out std_logic    
     );
 end Unita_Controllo_B_Completo;
 
 architecture Behavioral of Unita_Controllo_B_Completo is
 
-    type STATIB is (IDLE, CARICO_DATI, WAIT_OKUSER, SEND_ACK, ACK0);
+    type STATIB is (IDLE, DISPONIBILE, CARICO_DATI, RISPONDO, CHIUDO, ATTESA_RIS);
     
     signal stato_prossimo : STATIB;
     signal stato : STATIB;
@@ -67,39 +69,44 @@ begin
     begin
         ack <= '0';
         load <= '0';
+        done <= '0';
+        user_ack <= '0';    
         stato_prossimo <= stato;
         
         case stato is
         
             when IDLE =>   
-                if req = '1' then
+                if req = '1' & okUser='1' then
                     stato_prossimo <= CARICO_DATI;
                 else
                     stato_prossimo <= IDLE;
                 end if;
+                    
+            when DISPONIBILE =>
+                ack <= '1';
+                user_ack <= '1';
+                stato_prossimo <= CARICO_DATI;
             
             when CARICO_DATI =>
                 load <= '1'; -- “Sto acquisendo il dato che A ha appena reso valido”.
                 stato_prossimo <= WAIT_OKUSER;
                 
-           when WAIT_OKUSER => -- "Aspetto che A mi dica che il dato è stabile e posso leggerlo".
-                if okUser = '1' then
-                    stato_prossimo <= SEND_ACK;
+           when RISPONDO => -- "Aspetto che A mi dica che il dato è stabile e posso leggerlo".
+                done <= '1';
+                if req = '1' then
+                    stato_prossimo <= RISPONDO;
+                else
+                    stato_prossimo <= CHIUDO;
                 end if;
                 
-            when SEND_ACK => -- acquisizione vera e propria
-                ack <= '1'; -- ack = 1 → "Ho ricevuto e processato il dato"
-                if req = '0' then
-                    stato_prossimo <= ACK0;
-                else
-                    stato_prossimo <= SEND_ACK;
-                end if;
-             
-            when ACK0 =>
-                ack <= '0'; -- quando ho finito → "Sono pronto al prossimo dato"
-                if req = '0' then
-                    stato_prossimo <= IDLE;   
-                end if; 
+            when CHIUDO => -- acquisizione vera e propria
+                done <= '0';
+                stato_prossimo <= ATTESA_RIS;
+
+            when ATTESA_RIS =>
+                stato_prossimo <= ATTESA_RIS;
+
+                
         end case;
     end process;     
 
