4bf6e0bed8634e38b8f4d86774ed56a0f09593f0
Fabrizio <144843999+Fabronsky@users.noreply.github.com>
2026-02-24 18:50:53 +0100

    Update AutomaMoore.vhd

--- Esercizio 3 - Riconoscitore di sequenze/3.1/Moore doppio processo/AutomaMoore.vhd
+++ Esercizio 3 - Riconoscitore di sequenze/3.1/Moore doppio processo/AutomaMoore.vhd
@@ -12,7 +12,7 @@ end entity;
 
 architecture behavioral of AutomaMoore is
 
-    type stato is (S0, S1, S2, S3);
+    type stato is (S0, S1, S2, S3, N0, N1, N2);
     signal statoCorrente: stato := S0;
     signal statoProssimo: stato := S0; 
     
@@ -34,27 +34,56 @@ begin
             when S0 =>
                 Y <= '0';
                 if i = '0' then
-                    statoProssimo <= S0;
+                    statoProssimo <= N0;
                 else
                     statoProssimo <= S1;
                 end if;
             when S1 =>
                 Y <= '0';
                 if i = '0' then
-                    statoProssimo <= S0;
+                    statoProssimo <= N1;
                 else
                     statoProssimo <= S2;
                 end if;
             when S2 =>
                 Y <= '0';
                 if i = '0' then
-                    statoProssimo <= S0;
+                    statoProssimo <= N2;
                 else
                     statoProssimo <= S3;
                 end if;
+                    
             when S3 =>
                 Y <= '1';
-                statoProssimo <= S0;
+                if i = '0' then
+                    statoProssimo <= NO;
+                else
+                    statoProssimo <= S1;
+                end if;
+                    
+            when N0 =>
+                Y <= '0';
+                if i = '0' then
+                    statoProssimo <= N1;
+                else
+                    statoProssimo <= N1;
+                end if;  
+                    
+            when N1 =>
+                Y <= '0';
+                if i = '0' then
+                    statoProssimo <= N2;
+                else
+                    statoProssimo <= N2;
+                end if;
+                    
+            when N2 =>
+                Y <= '0';
+                if i = '0' then
+                    statoProssimo <= NO;
+                else
+                    statoProssimo <= S1;
+                end if;
         end case;
         
     end PROCESS;
