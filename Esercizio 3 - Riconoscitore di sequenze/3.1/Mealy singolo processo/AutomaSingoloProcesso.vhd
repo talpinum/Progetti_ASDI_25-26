a67ec727c14b131042ea657ac8f0ca37f3951eeb
Fabrizio <144843999+Fabronsky@users.noreply.github.com>
2026-02-24 18:44:22 +0100

    Update AutomaSingoloProcesso.vhd

--- Esercizio 3 - Riconoscitore di sequenze/3.1/Mealy singolo processo/AutomaSingoloProcesso.vhd
+++ Esercizio 3 - Riconoscitore di sequenze/3.1/Mealy singolo processo/AutomaSingoloProcesso.vhd
@@ -12,32 +12,56 @@ end entity;
 
 architecture behavioral of AutomaSingoloProcesso is
 
-    type stato is (S0, S1, S2);
+    type stato is (S0, S1, S2, S3, S4);
     signal statoCorrente: stato := S0;
     
 begin
 
     processo_sequenziale: PROCESS (A, RST)
     begin
+    if rising_edge(clk) then
         if RST = '1' then
             statoCorrente <= S0;
             Y <= '0';
-        elsif rising_edge(A) then
-            if (i = '0') then
-            statoCorrente <= S0;
-            Y <= '0';
-            else
-                case statoCorrente is
-                    when S0 =>
-                        statoCorrente <= S1;
-                        Y <= '0';
-                    when S1 =>
-                        statoCorrente <= S2;
-                        Y <= '0';
-                    when S2 =>
-                        statoCorrente <= S0;
-                        Y <= '1';
-                end case;
+        else
+         case statoCorrente is
+
+            when S0 =>
+                if (i = '0') then
+                    statoProssimo <= S3;
+                    Y <= '0';
+                else
+                    statoProssimo <= S1;
+                    Y <= '0';
+                end if;
+
+            when S1 =>
+                if (i = '0') then
+                    statoProssimo <= S4;
+                    Y <= '0';
+                else
+                    statoProssimo <= S2;
+                    Y <= '0';
+                end if;
+                    
+            when S2 =>
+                if (i = '0') then
+                    statoProssimo <= S0; 
+                    Y <= '0';
+                else
+                    statoProssimo <= S0; -- Sequenza 111
+                    Y <= '1';
+                end if;   
+                    
+            when S3 =>
+                    statoProssimo <= S4;
+                    Y <= '0';    
+
+            when S4 =>
+                    statoProssimo <= S0;
+                    Y <= '0';
+
+        end case;
             end if; 
         end if;
 end PROCESS;
